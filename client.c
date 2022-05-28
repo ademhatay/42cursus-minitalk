501b6a31c791d471eefe8872c460b64f51f739ea
Adem Hatay <hatayadem5@gmail.com>
2022-05-28 09:07:37 +0300

    new version

--- client.c
+++ client.c
@@ -1,76 +1,55 @@
-#include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
 
-static void	ft_send_msg(int pid, char*str)
+void    ft_putchar(const char c)
 {
-	size_t	i;
-
-	i = 0;
-	while (str[i] != 0)
-	{
-		if (str[i] == '1')
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		i++;
-		usleep(80);
-	}
+        write(1, &c, 1);
 }
 
-static char	*ft_to_bit(char *str, size_t i, size_t j)
+void    ft_putstr(const char *str)
 {
-	char	*ret;
-	int		c;
-	int		bytes;
-
-	i = strlen(str);
-	ret = calloc(i * 8 + 1, sizeof(char));
-	if (ret == NULL)
-		return (NULL);
-	while (i + 1 != 0)
-	{
-		c = str[i - 1];
-		bytes = 8;
-		while (bytes > 0)
-		{
-			if (c % 2 == 1)
-				ret[strlen(str) * 8 - j - 1] = '1';
-			else
-				ret[strlen(str) * 8 - j - 1] = '0';
-			c = c / 2;
-			j++;
-			bytes--;
-		}
-		i--;
-	}
-	return (ret);
+        while (*str)
+        {
+                ft_putchar(*str);
+                str++;
+        }
 }
 
-int	main(int ac, char **av)
+void    ft_putnbr(int nb)
 {
-	int		pid;
-	char	*bits;
+        if (nb == -2147483648)
+        {
+                ft_putchar('-');
+                ft_putchar('2');
+                ft_putnbr(147483648);
+                return ;
+        }
+        else if (nb >= 10)
+        {
+                ft_putnbr(nb / 10);
+                ft_putnbr(nb % 10);
+        }
+        else if (nb < 0)
+        {
+                nb = -nb;
+                ft_putchar('-');
+                ft_putnbr(nb);
+        }
+        else
+                ft_putchar(nb + '0');
+}
 
-	if (ac < 3)
-	{
-		printf("Eksik Arguman Girdiniz...");
-		return (0);
-	}
-	else if (ac > 3)
-	{
-		printf("Fazla Arguman Girdiniz...");
-		return (0);
-	}
-	pid = atoi(av[1]);
-	bits = ft_to_bit(av[2], 0, 0);
-	if (bits == NULL)
-	{
-		printf("Gerekli alan hafizadan tahsis edilemedi...");
-		return (0);
-	}
-	ft_send_msg(pid, bits);
-	free(bits);
+void    ft_putendl(const char *str)
+{
+        if(str)
+        {
+                ft_putstr(str);
+                ft_putchar('\n');
+        }
+}
+
+int     main(int ac, char **av)
+{
+	int
+        return (1);
 }
--- server.c
+++ server.c
@@ -1,66 +1,76 @@
-#include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
 
-
-// converts binary to ascii and prints it
-
-static void	ft_convert(char *s)
+void    ft_putchar(const char c)
 {
-	int				pow;
-	unsigned char	c;
-	size_t			i;
-
-	pow = 1;
-	c = 0;
-	i = strlen(s) - 1;
-	while (i + 1 != 0)
-	{
-		c += pow * (s[i] - '0');
-		pow = pow * 2;
-		i--;
-	}
-	write(1, &c, 1);
+        write(1, &c, 1);
 }
 
-// reads every bit and appends it to bits
-// if its a 8 bit it prints the char to the console
-
-static void	ft_confirm(int sig)
+void    ft_putstr(const char *str)
 {
-	static char	*bits;
-	static int	bitcount;
-    
-    
-	bitcount++;
-	if (bits == NULL)
-	{
-		bits = strdup("");
-		bitcount = 1;
-	}
-	if (sig == SIGUSR2)
-		bits[bitcount - 1] = '0';
-	else
-        bits[bitcount - 1] = '1';
-	if (bitcount == 8)
-	{
-		ft_convert(bits);
-		free(bits);
-		bits = NULL;
-	}
+        while (*str)
+        {
+                ft_putchar(*str);
+                str++;
+        }
 }
 
+void    ft_putnbr(int nb)
+{
+        if (nb == -2147483648)
+        {
+                ft_putchar('-');
+                ft_putchar('2');
+                ft_putnbr(147483648);
+                return ;
+        }
+        else if (nb >= 10)
+        {
+                ft_putnbr(nb / 10);
+                ft_putnbr(nb % 10);
+        }
+        else if (nb < 0)
+        {
+                nb = -nb;
+                ft_putchar('-');
+                ft_putnbr(nb);
+        }
+        else
+                ft_putchar(nb + '0');
+}
+
+void    ft_putendl(const char *str)
+{
+        if(str)
+        {
+                ft_putstr(str);
+                ft_putchar('\n');
+        }
+}
+
+void    ft_print_server_menu(int pid)
+{
+        ft_putchar('\n');
+        ft_putendl("\t\t\t\x1b[32mMINITALK\x1b[0m\t");
+        ft_putendl("\t\t\x1b[32m=========================\t\t\x1b[0m");
+        ft_putendl("\t\t     script by \x1b[36mahatay\x1b\t\n");
+        ft_putendl("\t\t\x1b[31mthis project made with love.\x1b\t");
+        ft_putendl("\t\t\x1b[32m=========================\t\t\x1b[0m");
+        ft_putstr("\t    \x1b[34mnumber for connect to Client: \x1b[0m");
+        ft_putnbr(pid);
+        ft_putchar('\n');
+        ft_putendl("\t\t\x1b[32m=========================\t\t\x1b[0mi\n");
+}
 
 int	main(void)
 {
-	printf("Senin PID'in: %d\n", getpid());
-	while (1)
+	int	pid;
+
+	pid = getpid();
+	ft_print_server_menu(pid);
+	while (42)
 	{
-		signal(SIGUSR1, ft_confirm);
-		signal(SIGUSR2, ft_confirm);
 		pause();
 	}
-	return (0);
+	return (1);
 }
